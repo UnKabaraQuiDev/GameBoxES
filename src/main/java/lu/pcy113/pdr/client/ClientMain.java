4f3d5d271450a416b18f5064e9bbdd2980bce177
P.Cy.113 <poucy113@school.lu>
2023-12-24 16:29:54 +0100

    added stderr redirection

--- src/main/java/lu/pcy113/pdr/client/ClientMain.java
+++ src/main/java/lu/pcy113/pdr/client/ClientMain.java
@@ -1,8 +1,14 @@
 package lu.pcy113.pdr.client;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.logging.Level;
 
@@ -16,6 +22,8 @@ public class ClientMain {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
 		System.out.println(Arrays.toString(new File("./config/").list()));
+
+		redirectStdErr();
 
 		GlobalLogger.init(new File("./config/logs.properties"));
 
@@ -38,6 +46,44 @@ public class ClientMain {
 
 		GlobalLogger.log(Level.INFO, "Stopped after " + (System.currentTimeMillis() - start) + "ms");
 
+	}
+
+	public static void redirectStdErr() {
+		// Create a pipe
+		PipedOutputStream pipeOut = new PipedOutputStream();
+		PipedInputStream pipeIn;
+		try {
+			pipeIn = new PipedInputStream(pipeOut);
+			System.setErr(new PrintStream(pipeOut));
+
+			final FileWriter writer = new FileWriter("./logs/error.log", false);
+			writer.write("started\n");
+			Thread readerThread = new Thread(() -> {
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(pipeIn))) {
+					String line;
+					while ((line = reader.readLine()) != null) {
+						System.out.println("Stderr: " + line);
+						writer.write("trace: " + System.currentTimeMillis() + line + "\n");
+						writer.flush();
+					}
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}, "Reader Thread");
+			readerThread.start();
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				try {
+					writer.write("ended\n");
+					writer.flush();
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
