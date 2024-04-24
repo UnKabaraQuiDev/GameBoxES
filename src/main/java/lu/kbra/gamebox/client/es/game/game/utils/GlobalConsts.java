e5ddc0eb261c326dee2a69641a46a7deaf251c5e
P.Cy.113 <poucy113@github.com>
2024-04-24 19:39:50 +0200

    Added GlobalConsts

---
+++ src/main/java/lu/kbra/gamebox/client/es/game/game/utils/GlobalConsts.java
@@ -0,0 +1,14 @@
+package lu.kbra.gamebox.client.es.game.game.utils;
+
+import org.joml.Vector4f;
+
+public final class GlobalConsts {
+
+	public static final Vector4f PRIMARY_DARK = new Vector4f(86f, 159f, 217f, 255f).div(255f);
+	public static final Vector4f PRIMARY_LIGHT = new Vector4f(153f, 198f, 233f, 255f).div(255f);
+	public static final Vector4f SECONDARY_DARK = new Vector4f(76f, 199f, 119f, 255f).div(255f);
+	public static final Vector4f SECONDARY_LIGHT = new Vector4f(160f, 232f, 153f, 255f).div(255f);
+	public static final Vector4f HIGHLIGHT = new Vector4f(255f, 126f, 0f, 255f).div(255f);
+	public static final Vector4f BG = new Vector4f(70f, 70f, 70f, 255f).div(255f);
+
+}
