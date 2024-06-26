0bf41eb234b00103026b17e8a8f233957e87a3a8
UnKabaraQuiDev <unkabaraquidev@github.com>
2024-06-26 19:23:50 +0200

    Updated windows update script to remove local changed

--- install/update_windows.bat
+++ install/update_windows.bat
@@ -1,10 +1,10 @@
 @echo off
 
-IF exist pclib (echo 1/3 PCLib already installed, updating & cd pclib & git clean -fd & git pull & cd ../) ELSE (echo 1/3 Installing PCLib & git clone https://github.com/UnKabaraQuiDev/pclib)
+IF exist pclib (echo 1/3 PCLib already installed, updating & cd pclib & git reset --hard & git pull & cd ../) ELSE (echo 1/3 Installing PCLib & git clone https://github.com/UnKabaraQuiDev/pclib)
 
-IF exist jbcodec (echo 2/3 JBcodec already installed, updating & cd jbcodec & git clean -fd & git pull & cd ../) ELSE (echo 2/3 Installing JBCodec & git clone https://github.com/UnKabaraQuiDev/jbcodec)
+IF exist jbcodec (echo 2/3 JBcodec already installed, updating & cd jbcodec & git reset --hard & git pull & cd ../) ELSE (echo 2/3 Installing JBCodec & git clone https://github.com/UnKabaraQuiDev/jbcodec)
 
-IF exist GameBoxES (echo 3/3 GameBoxES already installed, updating & cd GameBoxES & git clean -fd & git pull & cd ../) ELSE (echo 3/3 Installing GameBoxES & git clone https://github.com/UnKabaraQuiDev/GameBoxES)
+IF exist GameBoxES (echo 3/3 GameBoxES already installed, updating & cd GameBoxES & git reset --hard & git pull & cd ../) ELSE (echo 3/3 Installing GameBoxES & git clone https://github.com/UnKabaraQuiDev/GameBoxES)
 
 echo 1/3 Building PCLib
 cd pclib & call mvn clean package install:install & cd ../
