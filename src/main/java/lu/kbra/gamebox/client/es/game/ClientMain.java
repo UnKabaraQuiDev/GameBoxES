931acda16016a9702b08209a65f6eef68f982b7b
UnKabaraQuiDev <poucy113@github.com>
2024-06-26 09:32:49 +0200

    Removed useless println in ClientMain

--- src/main/java/lu/kbra/gamebox/client/es/game/ClientMain.java
+++ src/main/java/lu/kbra/gamebox/client/es/game/ClientMain.java
@@ -3,21 +3,17 @@ package lu.kbra.gamebox.client.es.game;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.logging.Level;
-
-import lu.pcy113.pclib.PCUtils;
-import lu.pcy113.pclib.logger.GlobalLogger;
 
 import lu.kbra.gamebox.client.es.engine.GameEngine;
 import lu.kbra.gamebox.client.es.engine.graph.window.WindowOptions;
 import lu.kbra.gamebox.client.es.game.game.GameBoxES;
+import lu.pcy113.pclib.PCUtils;
+import lu.pcy113.pclib.logger.GlobalLogger;
 
 public class ClientMain {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		System.out.println(Arrays.toString(new File("./config/").list()));
-
 		System.out.println(PCUtils.recursiveTree("./resources"));
 
 		GlobalLogger.init(new File("./config/logs.properties"));
